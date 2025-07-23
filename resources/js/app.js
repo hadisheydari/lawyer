88d25b196ca9bbb9921b8171c0c07e1e5ec6c1bd
hadisheydari <hadis.uix@gmail.com>
2025-07-23 11:12:34 +0330

    new

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,24 +1,6 @@
-import $ from 'jquery';
-
-// حتما اول jQuery رو به window اضافه کن
-window.$ = window.jQuery = $;
-
-// بعد select2 رو ایمپورت کن (بدون {} چون default export نیست)
-import 'select2';
-import 'select2/dist/css/select2.min.css';
-
 import '../css/app.css';
 import Alpine from 'alpinejs';
-import './chart.js';
-
-document.addEventListener('DOMContentLoaded', () => {
-    if ($('.select2').length) {
-        $('.select2').select2({
-            width: '100%',
-            dir: 'rtl',
-        });
-    }
-});
+import './chart.js'
 
 if (!window.Livewire) {
     window.Alpine = Alpine;
