be9c827d58c6ac259464ecdb3e2402f80f9f25e0
hadisheydari <hadis.uix@gmail.com>
2025-08-09 15:55:13 +0330

    crgos type

--- app/Http/Requests/CargoDeclaration/CargoReservationRequest.php
+++ app/Http/Requests/CargoDeclaration/CargoReservationRequest.php
@@ -1,18 +1,26 @@
 <?php
 
-namespace App\Http\Requests;
+namespace App\Http\Requests\CargoDeclaration;
 
 use Illuminate\Foundation\Http\FormRequest;
-use Illuminate\Validation\Rule;
-use App\Models\ReservationStatus;
+use App\Enums\Cargo\ReservationStatus;
+
 
 class CargoReservationRequest extends FormRequest
 {
-    public function authorize()
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
     {
         return true;
     }
 
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
     public function rules()
     {
         return [
